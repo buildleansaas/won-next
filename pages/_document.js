6a7e50952a559834af33263d052b18379bde3953
awitherow <au.witherow@gmail.com>
2018-04-30 17:22:36 +0900

    mobile tweaks

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="stylesheet" href="/_next/static/style.css" />
           <link
             rel="stylesheet"
