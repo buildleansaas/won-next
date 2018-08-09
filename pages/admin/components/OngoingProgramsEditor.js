847ab3e382d0acfb28278fba9f78b048e4749688
awitherow <au.witherow@gmail.com>
2018-08-09 13:58:59 -0400

    simplified not perfect tab interface

---
+++ pages/admin/components/OngoingProgramsEditor.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class OngoingProgramsEditor extends Component {
+  render() {
+    return (
+      <div className="panel">
+        <p>This is the panel to edit the Ongoing Programs</p>
+      </div>
+    );
+  }
+}
--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
 import Link from "next/link";
 
+import "./index.css";
+
 import t from "../../locale";
+
+import OngoingProgramsEditor from "./components/OngoingProgramsEditor";
+
+const tabs = [
+  {
+    text: "Ongoing Programs",
+    id: "ongoing-programs"
+  },
+  {
+    text: "Upcoming Events",
+    id: "upcoming-events"
+  }
+];
 
 export default class Home extends Component {
   state = {
     locale: "en",
-    loading: true
+    activeTabId: "ongoing-programs"
   };
 
   componentDidMount() {
@@ -27,12 +42,32 @@ export default class Home extends Component {
     );
   };
 
+  renderEditor(activeTabId) {
+    switch (activeTabId) {
+      case "ongoing-programs":
+        return <OngoingProgramsEditor />;
+
+      case "upcoming-events":
+        return (
+          <div className="panel">
+            <p>This is the panel to edit the Upcoming Events.</p>
+          </div>
+        );
+    }
+
+    return (
+      <div className="panel">
+        <p>This text should not be shown, please call Austin if so.</p>
+      </div>
+    );
+  }
+
   render() {
-    const { locale } = this.state;
+    const { locale, activeTabId } = this.state;
     const { header, footer, info } = t[locale];
 
     return (
-      <div className="">
+      <div className="admin">
         <nav>
           <ul className="header-links right thin">
             <li>
@@ -40,6 +75,19 @@ export default class Home extends Component {
             </li>
           </ul>
         </nav>
+
+        <div className="tabs">
+          {tabs.map(({ text, id }) => (
+            <button
+              onClick={() => this.changeState("activeTabId", id)}
+              className={id === activeTabId ? "active" : "inactive"}
+            >
+              {text}
+            </button>
+          ))}
+        </div>
+
+        {this.renderEditor(activeTabId)}
       </div>
     );
   }
