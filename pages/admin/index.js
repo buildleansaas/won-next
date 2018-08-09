ca3d5cc47bb7b709bf0ae65f0d9a441910e793e1
awitherow <au.witherow@gmail.com>
2018-08-09 11:36:21 -0400

    add basic admin page

---
+++ pages/admin/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class AdminPanel extends Component {
+  render() {
+    return (
+      <div className="">
+        <p>Hello, admin page!</p>
+      </div>
+    );
+  }
+}
